3e10b1d3f3a3b32cf7135aa03ce79919a7696f72
jaewan shin <wodhks6307@naver.com>
2021-12-20 16:50:30 +0900

    commit3

---
+++ src/main/webapp/res/js/board/detail2.js
@@ -0,0 +1,14 @@
+var cmtListContainerElem = document.querySelector('#cmtListContainer');
+
+function getList(){
+    var url = '/board/cmt?iboard=' + cmtListContainerElem.dataset.iboard;
+    fetch(url).then(function (res){
+        return res.json();
+    }).then(function (data){
+        console.log(data);
+    }).catch(function (err){
+        console.error(err);
+    });
+}
+
+getList();
